0faaf93401f299f7fe9a033b1e531e73441b5898
AlyssonSilv <alyssonsamueldasilva6@gmail.com>
2024-12-13 11:15:11 -0300

    final

---
+++ assets/js/portifolio.js
@@ -0,0 +1,6 @@
+// Este arquivo pode ser usado para adicionar interatividade ao portfólio.
+// Por exemplo, animações ao rolar a página, exibição de detalhes de projetos, etc.
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfólio carregado com sucesso!");
+  });
+  
